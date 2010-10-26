bffc0e9297ae23ef701331772a359a86826b66d2
aly.shamsy <aly.shamsy@701c5031-9793-ad4f-f4d9-d7398babfd84>
2010-10-26 23:15:49 +0000

    addition to MatrixArithmetic header and cpp files

--- geometry/MatrixArithmetic.h
+++ geometry/MatrixArithmetic.h
@@ -4,11 +4,10 @@ class MatrixArithmetic {
 public:
 	MatrixArithmetic();
 	~MatrixArithmetic();
-	Matrix do_scalar_multiplicatio(int scalar, Matrix& current_matrix);
+	Matrix do_scalar_multiplication(int scalar, Matrix& current_matrix);
 	Matrix do_matrix_addition(Matrix& A, Matrix& B);
 	Matrix do_matrix_subtraction(Matrix& A, Matrix& B);
 	Matrix do_matrix_multiplication(Matrix& A, Matrix& B);
 	double get_dot_product(Matrix& A, Matrix& B);
 	Matrix get_cross_product(Matrix&A, Matrix& B);
-
 };
