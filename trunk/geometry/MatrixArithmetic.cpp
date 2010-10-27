82a88a118e5c5f0d60f4978be8eab1bbce9ebc41
aly.shamsy <aly.shamsy@701c5031-9793-ad4f-f4d9-d7398babfd84>
2010-10-27 23:37:05 +0000

    matrix arithmetic and matrix transformation code updates

--- trunk/geometry/MatrixArithmetic.cpp
+++ trunk/geometry/MatrixArithmetic.cpp
@@ -11,6 +11,75 @@ MatrixArithmetic::~MatrixArithmetic() {
 
 }
 
+bool MatrixArithmetic::is_matrix_square(Matrix& A) {
+	if(A.get_rows() == A.get_columns())
+		return true;
+	else
+		return false;
+}
+
+Matrix MatrixArithmetic::generate_square_matrix(Matrix& current_matrix) {
+	int number_of_rows = current_matrix.get_rows();
+	int number_of_columns = current_matrix.get_columns();
+
+	int size = min(number_of_rows, number_of_columns);
+	
+	Matrix square_matrix;
+	
+	for(int i = 0; i < size; i++) {
+		for(int j = 0; j < size; j++)
+			square_matrix.set_matrix_element(i, j, current_matrix.get_matrix_element(i, j));
+	}
+
+	return square_matrix;
+}
+
+Matrix MatrixArithmetic::generate_identity_matrix(int size) {
+	Matrix identity_matrix(0.0, size, size);
+
+	for(int i = 0; i < size; i++) {
+		for(int j = 0; j < size; j++) {
+			if(i == j)
+				identity_matrix.set_matrix_element(i, j, 1);
+		}
+	}
+
+	return identity_matrix;
+}
+
+Matrix MatrixArithmetic::generate_row_vector(Vertex& current_vertex) {
+	Matrix row_vector(0.0, 1, 3);
+
+	row_vector.set_matrix_element(0, 0, current_vertex.x);
+	row_vector.set_matrix_element(0, 1, current_vertex.y);
+	row_vector.set_matrix_element(0, 2, current_vertex.z);
+
+	return row_vector;
+}
+
+Matrix MatrixArithmetic::generate_column_vector(Vertex& current_vertex) {
+	Matrix column_vector(0.0, 3, 1);
+
+	column_vector.set_matrix_element(0, 0, current_vertex.x);
+	column_vector.set_matrix_element(1, 0, current_vertex.y);
+	column_vector.set_matrix_element(2, 0, current_vertex.z);
+
+	return column_vector;
+}
+
+Matrix MatrixArithmetic::get_matrix_transpose(Matrix& current_matrix) {
+	int rows = current_matrix.get_rows();
+	int columns = current_matrix.get_columns();
+	Matrix transpose(0.0, rows, columns);
+
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < columns; j++)
+			transpose.set_matrix_element(i, j, current_matrix.get_matrix_element(j, i));
+	}
+
+	return transpose;
+}
+
 Matrix MatrixArithmetic::do_scalar_multiplication(int scalar, Matrix& current_matrix) {
 	int rows = current_matrix.get_rows();
 	int columns = current_matrix.get_columns();
@@ -25,7 +94,7 @@ Matrix MatrixArithmetic::do_scalar_multiplication(int scalar, Matrix& current_ma
 	return scaled_matrix;
 }
 
-Matrix MatrixArithmetic::do_matrix_addition(Matrix& A, Matrix& B) {
+Matrix operator+(Matrix& A, Matrix& B) {
 	int rows_a = A.get_rows();
 	int columns_a = A.get_columns();
 
@@ -43,7 +112,7 @@ Matrix MatrixArithmetic::do_matrix_addition(Matrix& A, Matrix& B) {
 	return added_matrix;
 }
 
-Matrix MatrixArithmetic::do_matrix_subtraction(Matrix& A, Matrix& B) {
+Matrix operator-(Matrix& A, Matrix& B) {
 	int rows_a = A.get_rows();
 	int columns_a = A.get_columns();
 
@@ -61,7 +130,7 @@ Matrix MatrixArithmetic::do_matrix_subtraction(Matrix& A, Matrix& B) {
 	return subtracted_matrix;
 }
 
-Matrix MatrixArithmetic::do_matrix_multiplication(Matrix& A, Matrix& B) {
+Matrix operator*(Matrix& A, Matrix& B) {
 	int rows_a = A.get_rows();
 	int columns_a = A.get_columns();
 
@@ -108,4 +177,6 @@ Matrix MatrixArithmetic::get_cross_product(Matrix&A, Matrix& B) {
 
 	element = (A.get_matrix_element(0, 2)*B.get_matrix_element(0, 0)) - (A.get_matrix_element(0, 0)*B.get_matrix_element(0, 2));
 	cross_product.set_matrix_element(0, 0, element);
+
+	return cross_product;
 }
