06432825f0bdef863091808328f21d1bed19223d
aly.shamsy <aly.shamsy@701c5031-9793-ad4f-f4d9-d7398babfd84>
2010-10-27 23:37:05 +0000

    matrix arithmetic and matrix transformation code updates

--- geometry/MatrixArithmetic.h
+++ geometry/MatrixArithmetic.h
@@ -1,13 +1,22 @@
 #include "Matrix.h"
+#include "Vertex.h"
 
 class MatrixArithmetic {
 public:
 	MatrixArithmetic();
 	~MatrixArithmetic();
+
+	bool is_matrix_square(Matrix& A);
+	Matrix generate_square_matrix(Matrix& current_matrix);
+	Matrix generate_identity_matrix(int size);
+	Matrix generate_row_vector(Vertex& current_vertex);
+	Matrix generate_column_vector(Vertex& current_vertex);
+	Matrix get_matrix_transpose(Matrix& current_matrix);
+
 	Matrix do_scalar_multiplication(int scalar, Matrix& current_matrix);
-	Matrix do_matrix_addition(Matrix& A, Matrix& B);
-	Matrix do_matrix_subtraction(Matrix& A, Matrix& B);
-	Matrix do_matrix_multiplication(Matrix& A, Matrix& B);
+	friend Matrix operator+(Matrix& A, Matrix& B);
+	friend Matrix operator-(Matrix& A, Matrix& B);
+	friend Matrix operator*(Matrix& A, Matrix& B);
 	double get_dot_product(Matrix& A, Matrix& B);
 	Matrix get_cross_product(Matrix&A, Matrix& B);
 };
