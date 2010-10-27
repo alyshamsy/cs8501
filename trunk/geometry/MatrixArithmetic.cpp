9c6b0862461d831442ca81c8e2f458bb26c5b2ed
aly.shamsy <aly.shamsy@701c5031-9793-ad4f-f4d9-d7398babfd84>
2010-10-27 12:00:42 +0000

    matrix arithmetic modification

--- trunk/geometry/MatrixArithmetic.cpp
+++ trunk/geometry/MatrixArithmetic.cpp
@@ -32,16 +32,15 @@ Matrix MatrixArithmetic::do_matrix_addition(Matrix& A, Matrix& B) {
 	int rows_b = B.get_rows();
 	int columns_b = B.get_columns();
 
-	if(rows_a == rows_b && columns_a == columns_b) {
-		Matrix added_matrix(0.0, rows_a, columns_a);
+	//if(rows_a == rows_b && columns_a == columns_b) 
+	Matrix added_matrix(0.0, rows_a, columns_a);
 
-		for(int i = 0; i < rows_a; i++) {
-			for(int j = 0; j < columns_a; j++)
-				added_matrix.set_matrix_element(i, j, A.get_matrix_element(i, j)+B.get_matrix_element(i, j));
-		}
-
-		return added_matrix;
+	for(int i = 0; i < rows_a; i++) {
+		for(int j = 0; j < columns_a; j++)
+			added_matrix.set_matrix_element(i, j, A.get_matrix_element(i, j)+B.get_matrix_element(i, j));
 	}
+
+	return added_matrix;
 }
 
 Matrix MatrixArithmetic::do_matrix_subtraction(Matrix& A, Matrix& B) {
@@ -51,16 +50,15 @@ Matrix MatrixArithmetic::do_matrix_subtraction(Matrix& A, Matrix& B) {
 	int rows_b = B.get_rows();
 	int columns_b = B.get_columns();
 
-	if(rows_a == rows_b && columns_a == columns_b) {
-		Matrix subtracted_matrix(0.0, rows_a, columns_a);
+	//if(rows_a == rows_b && columns_a == columns_b) 
+	Matrix subtracted_matrix(0.0, rows_a, columns_a);
 
-		for(int i = 0; i < rows_a; i++) {
-			for(int j = 0; j < columns_a; j++)
-				subtracted_matrix.set_matrix_element(i, j, A.get_matrix_element(i, j)-B.get_matrix_element(i, j));
-		}
-
-		return subtracted_matrix;
+	for(int i = 0; i < rows_a; i++) {
+		for(int j = 0; j < columns_a; j++)
+			subtracted_matrix.set_matrix_element(i, j, A.get_matrix_element(i, j)-B.get_matrix_element(i, j));
 	}
+
+	return subtracted_matrix;
 }
 
 Matrix MatrixArithmetic::do_matrix_multiplication(Matrix& A, Matrix& B) {
@@ -70,19 +68,44 @@ Matrix MatrixArithmetic::do_matrix_multiplication(Matrix& A, Matrix& B) {
 	int rows_b = B.get_rows();
 	int columns_b = B.get_columns();
 
-	if(columns_a == rows_b) {
-		double element = 0.0;
-		Matrix multiplied_matrix(0.0, rows_a, columns_b);
+	//if(columns_a == rows_b) 
+	double element = 0.0;
+	Matrix multiplied_matrix(0.0, rows_a, columns_b);
 
-		for(int i = 0; i < rows_a; i++) {
-			for(int j = 0; j <columns_b; j++) {
-				for(int k = 0; k < columns_a; k++) {
-					element += A.get_matrix_element(i, k) * B.get_matrix_element(k, j);
-				}
-				multiplied_matrix.set_matrix_element(i, j, element);
-			}	
-		}
-
-		return multiplied_matrix;
+	for(int i = 0; i < rows_a; i++) {
+		for(int j = 0; j <columns_b; j++) {
+			for(int k = 0; k < columns_a; k++) {
+				element += A.get_matrix_element(i, k) * B.get_matrix_element(k, j);
+			}
+			multiplied_matrix.set_matrix_element(i, j, element);
+		}	
 	}
+
+	return multiplied_matrix;
+}
+
+double MatrixArithmetic::get_dot_product(Matrix& A, Matrix& B) {
+	double dot_product = 0.0;
+	int rows = A.get_rows();
+	int columns = A.get_columns();
+
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < columns; j++)
+			dot_product += A.get_matrix_element(i, j) * B.get_matrix_element(j, i);
+	}
+
+	return dot_product;
+}
+
+Matrix MatrixArithmetic::get_cross_product(Matrix&A, Matrix& B) {
+	Matrix cross_product(0.0, 3, 1);
+
+	double element = (A.get_matrix_element(0, 1)*B.get_matrix_element(0, 2)) - (A.get_matrix_element(0, 2)*B.get_matrix_element(0, 1));
+	cross_product.set_matrix_element(0, 0, element);
+
+	element = (A.get_matrix_element(0, 2)*B.get_matrix_element(0, 0)) - (A.get_matrix_element(0, 0)*B.get_matrix_element(0, 2));
+	cross_product.set_matrix_element(0, 0, element);
+
+	element = (A.get_matrix_element(0, 2)*B.get_matrix_element(0, 0)) - (A.get_matrix_element(0, 0)*B.get_matrix_element(0, 2));
+	cross_product.set_matrix_element(0, 0, element);
 }
